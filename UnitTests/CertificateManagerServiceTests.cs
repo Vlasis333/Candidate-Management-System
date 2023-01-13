1472cc97029b41bfc62fdb1e4b6df54eba9a9ac4
Vlasios Mavraganis <vlasisvv@outlook.com>
2023-01-13 16:06:11 +0200

    Added Unit Testing for the read of Certificates

---
+++ UnitTests/CertificateManagerServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using EFDataAccess.Data.Repositories;
+using EFDataAccess.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using EFDataAccess.Helpers;
+using EFDataAccess.Models;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class CertificateManagerServiceTests
+    {
+        private readonly ICertificateManagerRepository _certificateManagerRepository = new CertificateManagerRepository(new AppDBContext());
+
+        [TestMethod]
+        public async Task TestGetCertificate()
+        {
+            // Arrange
+            var certificateId = 1;
+
+            // Act
+            var certificate = await _certificateManagerRepository.GetCertificate(certificateId);
+
+            // Assert
+            Assert.IsNotNull(certificate);
+            Assert.AreEqual("Coding Bootcamp", certificate.Title);
+            Assert.AreEqual("CB", certificate.AssessmentTestCode);
+            Assert.AreEqual(true, certificate.Active);
+        }
+    }
+}
